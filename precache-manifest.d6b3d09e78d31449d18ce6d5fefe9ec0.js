85400f5feabbd737a805efe55138cfa027c9adc9
Mohit Gahlot <gahlot.mohit6@gmail.com>
2018-12-11 21:39:51 -0800

    Updates

---
+++ precache-manifest.d6b3d09e78d31449d18ce6d5fefe9ec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c217283ebd85e62e2555",
+    "url": "/sensor-app/static/js/runtime~main.c217283e.js"
+  },
+  {
+    "revision": "5a5fc2ea89acd1a5b59b",
+    "url": "/sensor-app/static/js/main.5a5fc2ea.chunk.js"
+  },
+  {
+    "revision": "ed25fbbe3d5667dc627b",
+    "url": "/sensor-app/static/js/1.ed25fbbe.chunk.js"
+  },
+  {
+    "revision": "5a5fc2ea89acd1a5b59b",
+    "url": "/sensor-app/static/css/main.05033c0b.chunk.css"
+  },
+  {
+    "revision": "c5647b97fba3a25fa13da85fbba18c54",
+    "url": "/sensor-app/index.html"
+  }
+];
